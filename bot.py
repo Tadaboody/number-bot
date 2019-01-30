a9865e91eff936829f085c89000e86d1e491acf8
Tomer Keren <tomer.keren.dev@gmail.com>
2019-01-30 22:01:23 +0200

    Encapsulate bot into class
    Closes #2

--- bot.py
+++ bot.py
@@ -3,8 +3,7 @@ from enum import Enum
 from pathlib import Path
 
 import aiohttp
-from discord import Message
-from discord.ext import commands
+import discord
 from word2number.w2n import word_to_num
 
 
@@ -19,40 +18,37 @@ class NumbersAPI:
         def non_random(cls):
             return [category for category in cls if category != cls.RANDOM]
 
-    async def get(self, number: int, category: 'NumbersAPI.Categories'):
+    async def get(self, number: int, category: "NumbersAPI.Categories"):
         if category == NumbersAPI.Categories.RANDOM:
             non_random_categories = self.Categories.non_random()
             random.shuffle(non_random_categories)
-            for cat in non_random_categories :
+            for cat in non_random_categories:
                 try:
                     return await self.__ask_trivia(number, cat)
                 except ValueError:
                     pass
         return await self.__ask_trivia(number, category)
-    
 
-    async def __ask_trivia(self, number, category: 'NumbersAPI.Categories'):
-        NUMBERS_API = 'http://numbersapi.com'
-        url = '/'.join([NUMBERS_API, str(number), category.value])
-        async with aiohttp.request('GET',url,params={'json': True}) as resp:
+    async def __ask_trivia(self, number, category: "NumbersAPI.Categories"):
+        NUMBERS_API = "http://numbersapi.com"
+        url = "/".join([NUMBERS_API, str(number), category.value])
+        async with aiohttp.request("GET", url, params={"json": True}) as resp:
             resp.raise_for_status()
             json = await resp.json()
-        if not json['found']:
+        if not json["found"]:
             raise ValueError(f"{number} is boooring")
         return json["text"]
 
 
-client = commands.Bot(command_prefix="#")
-
-
 FILE_DIR = Path(__file__).resolve().parent
-SECRETS_DIR = FILE_DIR/ "secrets"
+SECRETS_DIR = FILE_DIR / "secrets"
 
 
 def main():
     print("Reloading...")
     token_file = SECRETS_DIR / "token.txt"
-    client.run(token_file.read_text())
+    id_file = SECRETS_DIR / "client_id.txt"
+    Numbie(id_file.read_text()).run(token_file.read_text())
 
 
 def find_number(sentance: str) -> int:
@@ -60,33 +56,37 @@ def find_number(sentance: str) -> int:
         return word_to_num(sentance)
     except ValueError:  # No word that is a number
         import re
+
         match = re.search(r"(\d+)", sentance)
         if match:
             return match[0]
     raise ValueError("No number found in sentence")
 
 
-@client.event
-async def on_message(message: Message):
-    if message.author == client.user:
-        return
-    try:
-        num = find_number(message.content)
-        print(f"number found {num}")
-        api = NumbersAPI()
-        resp = await api.get(num, api.Categories.RANDOM)
-        print(resp)
-        await client.send_message(message.channel, resp)
-    except ValueError as e:
-        print(e)
+class Numbie(discord.Client):
+    def __init__(self, client_id: str,**kwargs):
+        self.client_id = client_id
+        super().__init__(**kwargs)
 
+    async def on_message(self, message: discord.Message):
+        print(self.user.display_name)
+        if message.author == self.user:
+            return
+        try:
+            num = find_number(message.content)
+            print(f"number found {num}")
+            api = NumbersAPI()
+            resp = await api.get(num, api.Categories.RANDOM)
+            print(resp)
+            await self.send_message(message.channel, resp)
+        except ValueError as e:
+            print(e)
 
-@client.event
-async def on_ready():
-    client_id = SECRETS_DIR / "client_id.txt"
-    permissions = 2048
-    invite_link = f"https://discordapp.com/oauth2/authorize?&client_id={client_id.read_text()}&scope=bot&permissions={permissions}"
-    print(f"Bot running! Invite me at {invite_link}")
+    async def on_ready(self):
+        client_id = SECRETS_DIR / "client_id.txt"
+        permissions = 2048
+        invite_link = f"https://discordapp.com/oauth2/authorize?&client_id={client_id.read_text()}&scope=bot&permissions={permissions}"
+        print(f"Bot running! Invite me at {invite_link}")
 
 
 if __name__ == "__main__":
